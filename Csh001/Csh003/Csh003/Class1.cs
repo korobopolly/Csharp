078b6df6dff42d9944376450f193b9d3a80d9aae
korobopolly <72500196+korobopolly@users.noreply.github.com>
2021-03-22 13:09:20 +0900

    실습 영상

---
+++ Csh001/Csh003/Csh003/Class1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csh003
+{
+    class Class1
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Hello C# Programing!\n");
+
+            /*
+            int a = 273;
+            int b = 52;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+
+            
+            Console.WriteLine(1.0 + 2.0);
+            Console.WriteLine(1.0 - 2.0);
+            Console.WriteLine(1.0 * 2.0);
+            Console.WriteLine(1.0 / 2.0);
+            Console.WriteLine(1.0 % 2.0);
+            
+            Console.WriteLine('A');
+            Console.WriteLine('가');
+            Console.WriteLine('나');
+            
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("\"\"\"");
+            
+
+            Console.WriteLine("가나다" + "라마" + "바사아" + "자차카타" + "파하");
+            
+            Console.WriteLine("안녕하세요"[0]);
+            Console.WriteLine("안녕하세요"[1]);
+            Console.WriteLine("안녕하세요"[2]);
+            Console.WriteLine("안녕하세요"[3]);
+            Console.WriteLine("안녕하세요"[4]);
+
+            Console.WriteLine(!true);
+            Console.WriteLine(!false);
+            Console.WriteLine(!(52 < 273));
+            Console.WriteLine(!(52 > 273));
+            
+
+            Console.WriteLine(DateTime.Now.Hour < 9 || 18<DateTime.Now.Hour);
+            Console.WriteLine(9<DateTime.Now.Hour && DateTime.Now.Hour<18);
+            
+
+            int a = 2000000000;
+            int b = 1000000000;
+            Console.WriteLine(a + b);
+            
+
+            double a = 52.273;
+            double b = 103.32;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+            
+
+            char a = 'a';
+            Console.WriteLine(a);
+            
+
+            string message = "안녕하세요";
+            Console.WriteLine(message + "!");
+            Console.WriteLine(message[0]);
+            Console.WriteLine(message[1]);
+            Console.WriteLine(message[2]);
+            */
+
+            bool one = 10 < 0;
+            bool other = 20 > 100;
+
+            Console.WriteLine(one);
+            Console.WriteLine(other);
+
+        }
+    }
+}
