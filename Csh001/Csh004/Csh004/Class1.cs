f5145a8dacb728d86ed5fabca39626afc0d94555
korobopolly <72500196+korobopolly@users.noreply.github.com>
2021-03-26 11:38:08 +0900

    프로그래밍 기초

---
+++ Csh001/Csh004/Csh004/Class1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csh004
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int output = 0;
+            output += 52;
+            output += 273;
+            output += 103;
+
+            Console.WriteLine(output);
+            
+
+            string output = "hello ";
+            output += "world";
+            output += "!";
+
+            Console.WriteLine(output);
+            
+
+            int number = 10;
+            number++;
+            Console.WriteLine(number);
+            number--;
+            Console.WriteLine(number);
+            
+
+            int number = 10;
+            Console.WriteLine(number);
+            Console.WriteLine(number++);
+            Console.WriteLine(number--);
+            Console.WriteLine(number);
+            
+            int number = 10;
+            Console.WriteLine(number);
+            Console.WriteLine(++number);
+            Console.WriteLine(--number);
+            Console.WriteLine(number);
+            
+
+            int _int = 273;
+            long _long = 522731033265;
+            float _float = 52.273F;
+            double _double = 52.273;
+            char _char = '글';
+            string _string = "문자열";
+
+            Console.WriteLine(_int.GetType());
+            Console.WriteLine(_long.GetType());
+            Console.WriteLine(_float.GetType());
+            Console.WriteLine(_double.GetType());
+            Console.WriteLine(_char.GetType());
+            Console.WriteLine(_string.GetType());
+            
+
+            string input = Console.ReadLine();
+            Console.WriteLine("input: " + input);
+            
+
+            long longNumber = 2147483647L + 2147483647L;
+            int intNumber = (int)longNumber;
+            Console.WriteLine(intNumber);
+
+            Console.WriteLine(int.Parse("52"));
+            Console.WriteLine(long.Parse("273"));
+            Console.WriteLine(float.Parse("52.273"));
+            Console.WriteLine(double.Parse("103.32"));
+
+            Console.WriteLine(int.Parse("52").GetType());
+            Console.WriteLine(long.Parse("273").GetType());
+            Console.WriteLine(float.Parse("52.273").GetType());
+            Console.WriteLine(double.Parse("103.32").GetType());
+            
+
+            Console.WriteLine((52).ToString());
+            Console.WriteLine((52.273).ToString());
+            Console.WriteLine(('a').ToString());
+            Console.WriteLine((true).ToString());
+            Console.WriteLine((false).ToString());
+
+            Console.WriteLine((52).ToString().GetType());
+            Console.WriteLine((52.273).ToString().GetType());
+            Console.WriteLine(('a').ToString().GetType());
+            Console.WriteLine((true).ToString().GetType());
+            Console.WriteLine((false).ToString().GetType());
+
+            double number = 52.273103;
+            Console.WriteLine(number.ToString("0.0"));
+            Console.WriteLine(number.ToString("0.00"));
+            Console.WriteLine(number.ToString("0.000"));
+            Console.WriteLine(number.ToString("0.0000"));
+
+            Console.WriteLine(52 + 273);
+            Console.WriteLine("52" + 273); //문자열로 형변환 (문자열 연결 연산 수행)
+            Console.WriteLine(52 + "273");
+            Console.WriteLine("52" + "273");
+
+            char character = 'a';
+            string outputB = character + "";
+            Console.WriteLine(outputB.GetType()); //char형을 string으로 형변환
+            
+
+            Console.WriteLine(bool.Parse("True").GetType());
+            Console.WriteLine(bool.Parse("true").GetType());
+            Console.WriteLine(bool.Parse("False").GetType());
+            Console.WriteLine(bool.Parse("false").GetType());
+            */
+
+            //Console.WriteLine("미디어소프트웨어학과,4학년,윈도우프로그래밍[002],20161449,이준성");
+        }
+    }
+}
---
+++ Csh001/Csh005/Csh005/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csh005
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(bool.Parse("True"));
+            Console.WriteLine(bool.Parse("true"));
+            Console.WriteLine(bool.Parse("False"));
+            Console.WriteLine(bool.Parse("false"));
+            Console.WriteLine("미디어소프트웨어학과,4학년,윈도우프로그래밍[002],20161449,이준성");
+        }
+    }
+}
