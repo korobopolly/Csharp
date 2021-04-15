4d8da15cb325ef02ecb6e708d737a040b44de998
korobopolly <72500196+korobopolly@users.noreply.github.com>
2021-04-15 23:52:35 +0900

    크리스마스 트리 만들기

---
+++ Csh001/Csh008/Csh008/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csh008
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int k = 0; k < 7-i; k++)
+                {
+                    Console.Write(" ");
+                    
+                }
+                for (int k = 0; k < i + 1; k++)
+                {
+                    Console.Write("*");
+                }
+                for (int k = 0; k < i ; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
