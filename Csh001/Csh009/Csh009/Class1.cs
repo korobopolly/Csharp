9ce52ec94b6db20ba67a7086ee41cf07d8aa4d47
korobopolly <72500196+korobopolly@users.noreply.github.com>
2021-04-27 21:40:10 +0900

    클래스

---
+++ Csh001/Csh009/Csh009/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csh009
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("숫자를 입력해주세요: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("숫자를 입력해주세요: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("숫자를 입력해주세요: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("숫자를 입력해주세요: ");
+            int d = int.Parse(Console.ReadLine());
+            Console.WriteLine("---------------------");
+            int big = (a > b ? a : b);
+            int bigger= ( c> big ? c : big);
+            int biggger = (d > bigger ? d : bigger);
+
+            int small = (a < b ? a : b);
+            int smaller = (small < c ? small : c);
+            int smalller = (smaller < d ? smaller : d);
+
+            Console.WriteLine("가장 큰 수: "+biggger);
+            Console.WriteLine("가장 작은 수: "+smalller);
+        }
+    }
+}
---
+++ Csh001/Csh010/Csh010/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csh010
+{
+    class Class1
+    {
+        static void Main(String[] args)
+        {
+            /*
+            Random random = new Random();
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+
+            Console.WriteLine(random.NextDouble() * 10);
+            Console.WriteLine(random.NextDouble() * 10);
+            Console.WriteLine(random.NextDouble() * 10);
+            Console.WriteLine(random.NextDouble() * 10);
+            
+
+            List<int> list = new List<int>() { 52, 273, 32, 63 };
+
+            list.Remove(52);
+
+            foreach(var item in list)
+            {
+                Console.WriteLine("Count: " + list.Count + "\titem: " + item);
+            }
+            */
+
+            Console.WriteLine(Math.Abs(-52273)); //절대값
+            Console.WriteLine(Math.Ceiling(52.273)); //값보다 큰 최소 정수
+            Console.WriteLine(Math.Floor(52.273));
+            Console.WriteLine(Math.Max(52,273));
+            Console.WriteLine(Math.Min(52, 273));
+            Console.WriteLine(Math.Round(52.273));
+        }
+    }
+}
